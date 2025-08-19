eab218cb19123be79199e800a3bc7ef755e1a5e3
ley <ley@opera.com>
2025-08-19 14:45:27 +0800

    ✨ feat(go-concurrency): 添加端口扫描并发代码

---
+++ network-program/go-concurrency/scan-port/scanPort.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"go-concurrency/logger"
+	"net"
+	"strconv"
+	"sync"
+	"time"
+)
+
+var (
+	address = "127.0.0.1"
+	result  []string
+	resMux  sync.Mutex
+)
+
+func scanPort(port int, wg *sync.WaitGroup, ch chan bool) {
+	defer wg.Done()
+	fullAddr := address + ":" + strconv.Itoa(port)
+	logger.Debug("Start scan: %s", fullAddr)
+
+	conn, err := net.DialTimeout("tcp", fullAddr, 2*time.Second)
+	if err != nil {
+		<-ch
+		return
+	}
+	defer conn.Close()
+
+	resMux.Lock()
+	result = append(result, fmt.Sprintf("port: %d is open", port))
+	resMux.Unlock()
+
+	<-ch
+}
+
+func main() {
+
+	startPort := *flag.Int("start_port", 0, "start port")
+	endPort := *flag.Int("end_port", 1024, "end port")
+	workers := *flag.Int("num_workers", 32, "num of workers")
+
+	var wg sync.WaitGroup
+	concurr_chan := make(chan bool, workers)
+
+	for port := startPort; port <= endPort; port++ {
+		wg.Add(1)
+		concurr_chan <- true
+		go scanPort(port, &wg, concurr_chan)
+	}
+
+	wg.Wait()
+	for _, res := range result {
+		logger.Info(res)
+	}
+}
